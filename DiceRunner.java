4e16c2c51729ffe1c309e80dd31630dd0e077eae
RutuPatel221 <45392239+RutuPatel221@users.noreply.github.com>
2018-12-05 08:26:47 -0500

    Test 1
    Test one is completed.

--- DiceRunner.java
+++ DiceRunner.java
@@ -1,5 +1,25 @@
 public class DiceRunner{
 	public static void main(String[]args){
 
+		Die dice = new Die();
+		Die dice2 = new Die();
+
+		int counter = 0;
+		int sum = 0;
+
+		do{
+			System.out.println();
+			counter++;
+			dice.roll();
+			dice2.roll();
+
+			System.out.println("Roll " + counter + ": Die 1 " + dice.toString()+ "\t Die 2 " + dice2.toString());
+
+			sum = dice.getValue()+dice2.getValue();
+		}while(sum!=2);
+
+
+
+
 	}
 }
