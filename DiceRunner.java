76802ce19bd30e5d0d7e88d746e536d946c4ae53
SS1618 <45392178+SS1618@users.noreply.github.com>
2018-12-05 08:16:44 -0500

    test 2

--- DiceRunner.java
+++ DiceRunner.java
@@ -1,5 +1,13 @@
 public class DiceRunner{
 	public static void main(String[]args){
-
+		DiceHolder cup = new DiceHolder();
+		int getStat = 1;
+		while(getStat != -1){
+			Die die = new Die((int)(Math.random()*10)+1);
+			getStat = cup.addDie(die);
+			System.out.println(cup);
+		}
+		cup.shake();
+		System.out.println("After shaking cup: "+cup);
 	}
 }
